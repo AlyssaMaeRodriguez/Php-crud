00882780cbd7a5d0288205ed75e3bcdf2da06461
AlyssaMaeRodriguez <rodriguezalyssamae3@gmail.com.com>
2025-04-10 18:21:30 +0800

    alyssa

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include('header.php'); ?>
+            <h2>ALL STUDENTS</h2>
+            <table class="table table-hover table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>3</td>
+                <td>Alyssa Mae</td>
+                <td>Rodriguez</td>
+                <td>19</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Gellyn</td>
+                <td>Rabino</td>
+                <td>19</td>
+            </tr>
+            </tbody>
+        </table>
+        <?php include('footer.php'); ?>
